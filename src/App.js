4af96c815ee8b7cf85b09b039a759f2f9ce775ea
Axezious <ariealdiansyah98@gmail.com>
2020-09-02 23:12:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import  DataTable  from './dataTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { foods: [] };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/food/show')
+      .then(res => {
+        this.setState({ foods: res.data.DATA });
+        console.log(res.data.DATA);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  dataTable() {
+    return this.state.foods.map((data, i) => {
+      return <DataTable obj={data} key={i} />;
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrapper-users">
+        <div className="container">
+          <table className="table table-striped table-dark">
+            <thead className="thead-dark">
+              <tr>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Description</td>
+                <td>Picture</td>
+              </tr>
+            </thead>
+            <tbody>
+              {this.dataTable()}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//           <a className="navbar-brand">React Axios Tutorial</a>
+//         </nav>
+//       </header>
+
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-md-12">
+//             <p>Ini Body</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/dataTable.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class DataTable extends Component {
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.obj._id}
+                </td>
+                <td>
+                    {this.props.obj.name}
+                </td>
+                <td>
+                    {this.props.obj.description}
+                </td>
+                <td>
+                    {this.props.obj.picture.filename}
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default DataTable;
